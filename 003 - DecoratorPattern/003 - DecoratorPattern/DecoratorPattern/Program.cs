962fe1051deb42cf1d0479fa8cedbd4354f0d784
nick <nickswanenberg@gmail.com>
2025-09-18 14:17:51 +0200

    Condiments

---
+++ 003 - DecoratorPattern/003 - DecoratorPattern/DecoratorPattern/Program.cs
@@ -0,0 +1,33 @@
+﻿using Bev = DecoratorPattern.Beverages;
+using Cond = DecoratorPattern.Condiments;
+
+
+namespace DecoratorPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bev.Beverage espresso = new Bev.Espresso();
+            PrintBeverage(espresso);
+
+            Bev.Beverage lungo = new Bev.Espresso();
+            lungo = new Cond.Water(lungo);
+            PrintBeverage(lungo);
+
+            Bev.Beverage americano = new Bev.Espresso();
+            americano = new Cond.Water(americano);
+            americano = new Cond.Water(americano);
+            PrintBeverage(americano);
+
+            Bev.Beverage whiskey = new Bev.Water();
+            whiskey = new Cond.Whiskey(whiskey);
+            PrintBeverage(whiskey);
+        }
+
+        static void PrintBeverage(Bev.Beverage beverage)
+        {
+            Console.WriteLine(beverage.GetDescription() + " $" +  beverage.cost().ToString("#.##"));
+        }
+    }
+}
